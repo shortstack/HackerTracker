47175d7c2b67f6b4759b3ed4dfa966e37bb53a3a
Whitney Champion <whitney.champion@sparcedge.com>
2013-07-10 13:39:45 -0400

    twitter refactor, no auth, search works

--- src/com/shortstack/hackertracker/Activity/twitter.java
+++ src/com/shortstack/hackertracker/Activity/twitter.java
@@ -1,78 +1,84 @@
 package com.shortstack.hackertracker.Activity;
 
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import com.shortstack.hackertracker.Adapter.tweetAdapter;
-import com.shortstack.hackertracker.Model.tweet;
-import com.shortstack.hackertracker.R;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import com.shortstack.hackertracker.Adapter.tweetAdapter;
+import com.shortstack.hackertracker.R;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-public class twitter extends ListActivity {
+public class twitter extends Activity {
 
-
-    private ArrayList<tweet> tweets = new ArrayList<tweet>();
-
+    public ListView tweetListView;
+    public List<twitter4j.Status> tweets;
+    public twitter4j.Status tweetData[];
+    public tweetAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new MyTask().execute();
+        setContentView(R.layout.twitter);
+
+        List<twitter4j.Status> result = null;
+        try {
+           result = new getTweets().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ExecutionException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        if (!((result).size() < 1)) {
+            tweets = result;
+
+            tweetData = tweets.toArray(new twitter4j.Status[tweets.size()]);
+
+            adapter = new tweetAdapter(getApplicationContext(), R.layout.tweet, tweetData);
+
+            tweetListView = (ListView) findViewById(R.id.listView);
+
+            tweetListView.setAdapter(adapter);
+        }
+
     }
 
 
-    private class MyTask extends AsyncTask<Void, Void, Void> {
-        private ProgressDialog progressDialog;
 
-        protected void onPreExecute() {
-            progressDialog = ProgressDialog.show(twitter.this,
-                    "", "Loading. Please wait...", true);
-        }
+
+    public class getTweets extends AsyncTask<Void, Void, List<twitter4j.Status>> {
+
+        public List<twitter4j.Status> tweetList;
 
         @Override
-        protected Void doInBackground(Void... arg0) {
+        protected List<twitter4j.Status> doInBackground(Void... arg0) {
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("ZU1e66hITLMNn5RtVdSeSA")
+                    .setOAuthConsumerSecret("pvi8ZTGi6LgZdKjb04vSMXKa8Rb07V5fziyF8856PwY")
+                    .setOAuthAccessToken("8094902-wyRdtNsRgjYLO1EUPhGuHPyh52wUCFXgaGv291Lrlr")
+                    .setOAuthAccessTokenSecret("7SPrqk8Xe8rzYnZrCGrL7dCAe8vshmsfvNmGidDRM");
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
             try {
-                HttpClient hc = new DefaultHttpClient();
-                HttpGet get = new
-                        HttpGet("http://search.twitter.com/search.json?q=#defcon");
-                HttpResponse rp = hc.execute(get);
-                if(rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
-                {
-                    String result = EntityUtils.toString(rp.getEntity());
-                    JSONObject root = new JSONObject(result);
-                    JSONArray sessions = root.getJSONArray("results");
-                    for (int i = 0; i < sessions.length(); i++) {
-                        JSONObject session = sessions.getJSONObject(i);
-                        tweet tweet = new tweet();
-                        tweet.content = session.getString("text");
-                        tweet.author = session.getString("from_user");
-                        tweets.add(tweet);
-                    }
-                }
-            } catch (Exception e) {
-                Log.e("twitter", "Error loading JSON", e);
+                Query query = new Query("defcon");
+                QueryResult result;
+                result = twitter.search(query);
+                tweetList = result.getTweets();
+            } catch (TwitterException te) {
+                te.printStackTrace();
+                Log.v("tweet","Failed to search tweets: " + te.getMessage());
             }
-            return null;
+
+            return tweetList;
         }
 
-        @Override
-        protected void onPostExecute(Void result) {
-            progressDialog.dismiss();
-            setListAdapter(new tweetAdapter(
-                    twitter.this, twitter.this, R.layout.tweet, tweets));
-        }
+
     }
 
 
--- src/com/shortstack/hackertracker/Adapter/tweetAdapter.java
+++ src/com/shortstack/hackertracker/Adapter/tweetAdapter.java
@@ -19,30 +19,64 @@ import java.util.ArrayList;
  * Time: 9:20 AM
  * Description:
  */
-public class tweetAdapter extends ArrayAdapter<tweet> {
-    private ArrayList<tweet> tweets;
-    private Activity activity;
+public class tweetAdapter extends ArrayAdapter<twitter4j.Status> {
 
-    public tweetAdapter(Activity activity, Context context,
-                            int textViewResourceId,
-                            ArrayList<tweet> items) {
-        super(context, textViewResourceId, items);
-        this.tweets = items;
-        this.activity = activity;
+    Context context;
+    int layoutResourceId;
+    twitter4j.Status data[] = null;
+
+    public tweetAdapter(Context context, int layoutResourceId, twitter4j.Status[] data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View v = convertView;
-        if (v == null) {
-            LayoutInflater vi = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = vi.inflate(R.layout.tweet, null);
+        TweetHolder holder;
+        View row = convertView;
+
+        if ( row == null )
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new TweetHolder();
+            holder.userName = (TextView) row.findViewById(R.id.userName);
+            holder.tweet = (TextView) row.findViewById(R.id.tweet);
+            row.setTag(holder);
+
+
+        } else {
+            holder = (TweetHolder)row.getTag();
         }
-        tweet o = tweets.get(position);
-        TextView tt = (TextView) v.findViewById(R.id.toptext);
-        TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-        tt.setText(o.content);
-        bt.setText(o.author);
-        return v;
+
+        final twitter4j.Status tweet = data[position];
+
+
+        // if tweets in list, populate data
+        if (tweet.getText() != null) {
+
+            // set username
+            holder.userName.setText(tweet.getUser().getScreenName());
+
+            // set event title
+            holder.tweet.setText(tweet.getText());
+
+
+
+
+
+        } else {
+            holder.userName.setText("No tweets found");
+        }
+
+        return row;
+    }
+
+    static class TweetHolder {
+        TextView userName;
+        TextView tweet;
     }
 }
