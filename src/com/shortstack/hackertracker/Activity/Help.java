45894220a68ff6044292f8166fcc8152a1a75d89
shortstack <whitney.ellis.champion@gmail.com>
2013-07-15 01:01:58 -0400

    help section

---
+++ src/com/shortstack/hackertracker/Activity/Help.java
@@ -0,0 +1,22 @@
+package com.shortstack.hackertracker.Activity;
+
+import android.os.Bundle;
+import com.shortstack.hackertracker.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Whitney Champion
+ * Date: 7/14/13
+ * Time: 12:08 PM
+ */
+public class Help extends HackerTracker {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+
+
+    }
+}
