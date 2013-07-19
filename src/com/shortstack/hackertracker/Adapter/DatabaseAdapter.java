7c03f6f1afe69c9febcfe7e4d914f02c08432235
Whitney Champion <whitney.champion@sparcedge.com>
2013-07-19 15:44:14 -0400

    version increment

--- src/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
+++ src/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
@@ -41,7 +41,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
      */
     public DatabaseAdapter(Context context)  {
 
-        super(context, DB_NAME, null, 25);
+        super(context, DB_NAME, null, 26);
         this.myContext = context;
     }
 
