10e78553eeb0f7d9766cf554c3ac2b37413853c0
shortstack <whitney.ellis.champion@gmail.com>
2014-07-22 12:00:59 -0400

    schedule error fix

--- hackertracker/src/main/java/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
@@ -44,7 +44,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
      */
     public DatabaseAdapter(Context context)  {
 
-        super(context, DB_NAME, null, 109);
+        super(context, DB_NAME, null, 110);
         this.myContext = context;
     }
 
