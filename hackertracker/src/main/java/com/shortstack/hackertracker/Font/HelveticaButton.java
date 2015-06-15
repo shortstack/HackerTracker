f040a6a9cf2bb9334aecb096dac0ec8822c56548
Whitney Champion <whitney.ellis.champion@gmail.com>
2015-06-15 15:30:20 -0400

    custom fonts

---
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Font/HelveticaButton.java
@@ -0,0 +1,37 @@
+package com.shortstack.hackertracker.Font;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Whitney Champion on 6/15/15.
+ */
+public class HelveticaButton extends Button {
+
+    public HelveticaButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public HelveticaButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public HelveticaButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public void init() {
+
+        Typeface tf = Typeface.createFromAsset
+                (getContext().getAssets(), "font/HelveticaNeue-Thin.ttf");
+        setTypeface(tf ,1);
+
+    }
+}
+
