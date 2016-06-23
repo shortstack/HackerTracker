cd3825a778fb645f034ed3a567fd40c6efad42f3
Whitney Champion <whitney.ellis.champion@gmail.com>
2016-06-23 11:40:12 -0400

    database updates

--- hackertracker/src/main/java/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Adapter/DatabaseAdapter.java
@@ -27,7 +27,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 
     private static String DB_NAME = "hackertracker.sqlite";
 
-    private static int DB_VERSION = 246;
+    private static int DB_VERSION = 247;
 
     private SQLiteDatabase myDataBase;
 
