8f39bf0a12e118a01ad1efaea7b3ae704a5684d9
Chris <disenchanted@live.ca>
2016-12-21 17:08:52 -0800

    Converting Maps back to a Fragment'

--- hackertracker/src/main/java/com/shortstack/hackertracker/Activity/MapsActivity.java
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Activity/MapsActivity.java
@@ -2,23 +2,27 @@ package com.shortstack.hackertracker.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.joanzapata.pdfview.PDFView;
 import com.joanzapata.pdfview.listener.OnLoadCompleteListener;
+import com.orhanobut.logger.Logger;
+import com.shortstack.hackertracker.Alert.MaterialAlert;
 import com.shortstack.hackertracker.R;
+import com.shortstack.hackertracker.View.UberView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends Fragment {
 
     public static final String ASSET_NAME = "map_defcon_small.pdf";
-
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
 
     @Bind(R.id.viewer)
     PDFView pdfViewer;
@@ -26,41 +30,53 @@ public class MapsActivity extends AppCompatActivity {
     @Bind(R.id.progress_container)
     View progress;
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        ButterKnife.bind(this);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_maps, container, false);
+        ButterKnife.bind(this, view);
+        setHasOptionsMenu(true);
+        return view;
+    }
 
-        setSupportActionBar(toolbar);
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         pdfViewer.fromAsset(ASSET_NAME).onLoad(new OnLoadCompleteListener() {
             @Override
             public void loadComplete(int nbPages) {
                 progress.setVisibility(View.GONE);
             }
         }).load();
-
-
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if( pdfViewer != null )
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_uber:
+                MaterialAlert.create(getContext()).setTitle(R.string.uber).setView( new UberView(getContext())).show();
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.maps, menu);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Logger.d("onDestroy maps");
+        if( pdfViewer != null ) {
+            Logger.d("Recycling pdf.");
             pdfViewer.recycle();
+        }
+    }
+
+    public static Fragment newInstance() {
+        return new MapsActivity();
     }
 }
