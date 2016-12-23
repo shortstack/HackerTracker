c9c715501ac06b66513b37bbbeac62c68db82916
Chris <disenchanted@live.ca>
2016-12-22 22:24:03 -0800

    Removing tutorial pager from Splash

--- hackertracker/src/main/java/com/shortstack/hackertracker/Activity/SplashActivity.java
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Activity/SplashActivity.java
@@ -4,22 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.shortstack.hackertracker.Application.App;
-import com.shortstack.hackertracker.BuildConfig;
 import com.shortstack.hackertracker.R;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Whitney Champion
- * Date: 8/29/12
- * Time: 2:26 PM
- */
 public class SplashActivity extends AppCompatActivity {
 
-    public static final int SPLASH_DELAY = BuildConfig.DEBUG ? 0 : 450;
+    public static final int SPLASH_DELAY = 450;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,29 +21,9 @@ public class SplashActivity extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                startNextActivity();
+                startHomeActivity();
             }
         }, SPLASH_DELAY);
-    }
-
-
-
-    private void startNextActivity() {
-        boolean seenOnboarding = App.getStorage().seenOnboarding();
-
-        //seenOnboarding = !BuildConfig.DEBUG; // To force debug builds to see the onboarding.
-
-        if( seenOnboarding ) {
-            startHomeActivity();
-        } else {
-            App.getStorage().markOnboardingSeen();
-            startOnboardingActivity();
-        }
-    }
-
-    private void startOnboardingActivity() {
-        startActivity(new Intent(SplashActivity.this, TutorialActivity.class));
-        finish();
     }
 
     private void startHomeActivity() {
