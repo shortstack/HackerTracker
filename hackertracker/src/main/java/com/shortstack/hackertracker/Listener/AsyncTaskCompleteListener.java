13c6cbfc84852d89a488565bff75b3a3c5b11bce
Chris <disenchanted@live.ca>
2017-01-30 19:59:07 -0800

    Removing extra networking code

--- hackertracker/src/main/java/com/shortstack/hackertracker/Listener/AsyncTaskCompleteListener.java
+++
@@ -1,9 +0,0 @@
-package com.shortstack.hackertracker.Listener;
-
-/**
- * Created by Whitney Champion on 3/29/14.
- */
-public interface AsyncTaskCompleteListener<T> {
-
-    void onTaskComplete(T result);
-}
