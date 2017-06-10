1718a44f053ef3dbd268dc44bbcbf972afc7c7d0
Chris <disenchanted@live.ca>
2017-06-09 22:37:30 -0700

    Converting InformationFragment.

--- hackertracker/src/main/java/com/shortstack/hackertracker/Fragment/InformationFragment.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Fragment/InformationFragment.kt
@@ -1,24 +1,79 @@
 package com.shortstack.hackertracker.Fragment
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import butterknife.ButterKnife
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pedrogomez.renderers.RendererAdapter
+import com.pedrogomez.renderers.RendererBuilder
+import com.shortstack.hackertracker.Model.Information
 import com.shortstack.hackertracker.R
+import com.shortstack.hackertracker.Renderer.FAQRenderer
+import com.shortstack.hackertracker.Renderer.GenericHeaderRenderer
+import com.shortstack.hackertracker.Renderer.InformationRenderer
+import kotlinx.android.synthetic.main.fragment_recyclerview.*
 
-class InformationFragment : AppCompatActivity() {
+class InformationFragment : Fragment() {
 
-    private val ARG_LAYOUT_RES = "layout_res"
-    private val DEFAULT_LAYOUT = R.layout.alert_badges
+    var adapter: RendererAdapter<Any>? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_recyclerview, container, false)
+    }
 
-        var res = DEFAULT_LAYOUT
-        if (intent.extras != null) {
-            res = intent.extras.getInt(ARG_LAYOUT_RES, DEFAULT_LAYOUT)
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layout = LinearLayoutManager(context)
+        list!!.layoutManager = layout
+
+        val rendererBuilder = RendererBuilder<Any>()
+                .bind(Array<String>::class.java, FAQRenderer())
+                .bind(String::class.java, GenericHeaderRenderer())
+                .bind(Information::class.java, InformationRenderer())
+
+        adapter = RendererAdapter<Any>(rendererBuilder)
+        list!!.adapter = adapter
+
+
+        addInformationButtons()
+        addFAQ()
+    }
+
+    private fun addInformationButtons() {
+        adapter?.add(Information(context, R.array.location_information))
+        adapter?.add(Information(context, R.array.badge_information))
+        adapter?.add(Information(context, R.array.workshop_information))
+        adapter?.add(Information(context, R.array.wifi_information))
+        adapter?.add(Information(context, R.array.radio_information))
+        // radio
+        // workshop
+        // location/time
+        //
+    }
+
+    private fun addFAQ() {
+        adapter?.add("FAQ")
+
+        val myItems = resources.getStringArray(R.array.faq_questions)
+
+        var i = 0
+        while (i < myItems.size - 1) {
+            val update = arrayOfNulls<String>(2)
+
+            update[0] = myItems[i]
+            update[1] = myItems[i + 1]
+
+            adapter?.add(update)
+            i += 2
         }
+    }
 
-        setContentView(res)
-        ButterKnife.bind(this)
+    companion object {
+        fun newInstance(): InformationFragment {
+            return InformationFragment()
+        }
     }
 }
