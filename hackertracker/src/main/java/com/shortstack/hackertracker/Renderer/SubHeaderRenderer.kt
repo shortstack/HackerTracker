a4b5d633f769714e0053e796954a298a7c84fd12
Chris <disenchanted@live.ca>
2017-07-21 02:41:11 -0700

    Splitting time renderers

--- hackertracker/src/main/java/com/shortstack/hackertracker/Renderer/SubHeaderRenderer.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Renderer/SubHeaderRenderer.kt
@@ -3,19 +3,18 @@ package com.shortstack.hackertracker.Renderer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+
 import com.pedrogomez.renderers.Renderer
 import com.shortstack.hackertracker.R
-import com.shortstack.hackertracker.View.TimeView
-import kotlinx.android.synthetic.main.row_time_container.view.*
-import java.util.*
 
-class GenericTimeRenderer : Renderer<Date>() {
+class SubHeaderRenderer : Renderer<String>() {
 
     override fun inflate(inflater: LayoutInflater, parent: ViewGroup): View {
-        return inflater.inflate(R.layout.row_time_container, parent, false)
+        return inflater.inflate(R.layout.row_sub_header, parent, false)
     }
 
     override fun render(payloads: List<Any>) {
-        rootView.time_item.setDate(content)
+        (rootView as TextView).text = content
     }
 }
