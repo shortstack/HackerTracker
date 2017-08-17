2e703d1d82fdbb85a2ac4930e3e19c3f8cee958b
Chris <disenchanted@live.ca>
2017-08-17 02:13:19 -0700

    Cleaning Vendors Model

--- hackertracker/src/main/java/com/shortstack/hackertracker/Model/Vendor.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Model/Vendor.kt
@@ -1,13 +1,6 @@
 package com.shortstack.hackertracker.Model
 
-import android.content.ContentValues
-import android.database.Cursor
 import android.text.TextUtils
-import com.google.gson.Gson
-import com.orhanobut.logger.Logger
-import org.json.JSONException
-import org.json.JSONObject
-
 import java.io.Serializable
 
 class Vendors {
@@ -16,9 +9,9 @@ class Vendors {
 
     class Vendor : Serializable {
 
-        var title: String? = null
-        var description: String? = null
-        var link: String? = null
+        lateinit var title: String
+        lateinit var description: String
+        lateinit var link: String
         var partner: Int = 0
 
         val isPartner: Boolean
@@ -26,45 +19,6 @@ class Vendors {
 
         fun hasLink(): Boolean {
             return !TextUtils.isEmpty(link)
-        }
-
-        fun getContentValues(gson: Gson): ContentValues {
-            val values = ContentValues()
-
-            val json = gson.toJson(this)
-            try {
-                val `object` = JSONObject(json)
-
-                val keys = `object`.keys()
-                var key: String
-                while (keys.hasNext()) {
-                    key = keys.next()
-                    values.put(key, `object`.getString(key))
-                }
-
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-
-            return values
-        }
-
-        companion object {
-            fun CursorToCompany(gson: Gson, cursor: Cursor): Vendor {
-                val obj = JSONObject()
-
-                val totalColumn = cursor.columnCount
-
-                for (i in 0..totalColumn - 1) {
-                    try {
-                        obj.put(cursor.getColumnName(i), cursor.getString(i))
-                    } catch (e: Exception) {
-                        Logger.e(e, "Failed to convert Cursor into JSONObject.")
-                    }
-                }
-
-                return gson.fromJson(obj.toString(), Vendor::class.java)
-            }
         }
     }
 }
