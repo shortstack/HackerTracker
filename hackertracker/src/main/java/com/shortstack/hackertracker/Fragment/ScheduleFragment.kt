d3b692ccc0604a09882fceca9a67a5039519aa83
Chris <disenchanted@live.ca>
2017-08-19 03:45:08 -0700

    Adding infinte scrolling to schedule, improving adapter

--- hackertracker/src/main/java/com/shortstack/hackertracker/Fragment/ScheduleFragment.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Fragment/ScheduleFragment.kt
@@ -1,12 +1,12 @@
 package com.shortstack.hackertracker.Fragment
 
-import android.database.sqlite.SQLiteException
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,9 +18,8 @@ import com.shortstack.hackertracker.Common.Constants
 import com.shortstack.hackertracker.Event.RefreshTimerEvent
 import com.shortstack.hackertracker.Event.SyncResponseEvent
 import com.shortstack.hackertracker.Event.UpdateListContentsEvent
+import com.shortstack.hackertracker.List.ScheduleInfiniteScrollListener
 import com.shortstack.hackertracker.List.ScheduleItemAdapter
-import com.shortstack.hackertracker.Model.Filter
-import com.shortstack.hackertracker.Model.Item
 import com.shortstack.hackertracker.Network.DatabaseService
 import com.shortstack.hackertracker.Network.SyncRepository
 import com.shortstack.hackertracker.R
@@ -30,9 +29,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_schedule.*
 import kotlinx.android.synthetic.main.fragment_schedule.view.*
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
 
@@ -98,6 +94,7 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val rootView = inflater!!.inflate(contentView, container, false) as ViewGroup
         setHasOptionsMenu(true)
 
@@ -106,7 +103,23 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
         rootView.swipe_refresh!!.setOnRefreshListener(this)
         rootView.swipe_refresh!!.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimaryDark)
-        adapter = ScheduleItemAdapter()
+        adapter = ScheduleItemAdapter(layout, rootView.list)
+
+
+
+
+        rootView.list.addOnScrollListener(object : ScheduleInfiniteScrollListener(layout) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                val size = adapter.collection.size
+                adapter.load(page)
+                rootView.list.post {
+                    run {
+                        adapter.notifyItemRangeInserted(size, adapter.collection.size - size)
+                    }
+                }
+            }
+
+        })
         rootView.list!!.adapter = adapter
 
 
@@ -118,13 +131,8 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         refreshContents()
     }
 
-    private fun hasFilters(): Boolean {
-        val filter = App.storage.filter
-        return !filter.typesSet.isEmpty()
-    }
-
     private fun hasScheduleItems(): Boolean {
-        return !adapter!!.collection.isEmpty()
+        return !adapter.collection.isEmpty()
     }
 
     private fun updateFeedErrors() {
@@ -141,100 +149,22 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     }
 
     private fun refreshContents() {
-        adapter!!.clear()
+        val size = adapter.collection.size
+        adapter.clear()
+        adapter.notifyItemRangeRemoved(0, size )
 
-        val filter = App.application.storage.filter
-        val events = getEvents(filter)
 
-        val objects = addTimeDividers(events)
-        adapter!!.addAll(objects)
+        adapter.initContents()
 
         updateFeedErrors()
 
-
-        adapter!!.notifyDataSetChanged()
-
-        if (App.storage.showExpiredEvents())
-            scrollToCurrentTime()
     }
 
-    private fun scrollToCurrentTime() {
-        list!!.layoutManager.scrollToPosition(findCurrentPositionByTime())
-    }
-
-    private fun findCurrentPositionByTime(): Int {
-        val collection = adapter!!.collection
-        val currentDate = App.getCurrentDate()
-
-        for (i in collection.indices) {
-            val obj = collection[i]
-
-            if (obj is Item) {
-
-                val beginDateObject = obj.beginDateObject
-                if (beginDateObject.after(currentDate)) {
-                    for (i1 in i - 1 downTo 0) {
-//                        if (obj !is String) {
-//                            return i1
-//                        }
-                    }
-                    return i
-                }
-            }
-        }
-
-        return 0
-    }
-
-    protected fun getEvents(filter: Filter): List<Item> {
-        //        if( !hasFilters() ) {
-        //            return Collections.emptyList();
-        //        }
-
-        try {
-            val events: List<Item> = App.application.databaseController.getItemByDate(*filter.typesArray)
-            return events
-        } catch (ex: SQLiteException) {
-            Logger.e(ex, "Could not open the database.")
-            return emptyList()
-        }
-
-    }
-
-    private fun addTimeDividers(events: List<Item>): List<Any> {
-        val result = ArrayList<Any>()
-
-        if (events.isEmpty())
-            return result
-
-        result.add(events[0].dateStamp)
-        result.add(events[0].beginDateObject)
-
-        for (i in 0..events.size - 1 - 1) {
-            val current = events[i]
-
-            result.add(current)
-
-            val next = events[i + 1]
-            if (current.date != next.date) {
-                result.add(next.dateStamp)
-            }
-
-            if (current.begin != next.begin) {
-                result.add(next.beginDateObject)
-            }
-        }
-
-        result.add(events[events.size - 1])
-
-        return result
-    }
-
-    protected val contentView: Int
+    private val contentView: Int
         get() = R.layout.fragment_schedule
 
     fun showFilters() {
-        val filter = App.storage.filter
+        val filter = App.application.storage.filter
 
         val view = FilterView(context, filter)
         MaterialAlert.create(context).setTitle(getString(R.string.alert_filter_title)).setView(view).setBasicNegativeButton().setPositiveButton(R.string.save) { _, _ ->
@@ -246,11 +176,7 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
 
     override fun onRefresh() {
-        val retrofit = Retrofit.Builder().baseUrl(Constants.API_URL_BASE)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-        val service = retrofit.create(DatabaseService::class.java)
+        val service = DatabaseService.create()
 
         val syncRepository = SyncRepository(service)
         syncRepository.getSchedule()
@@ -262,7 +188,7 @@ class ScheduleFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
                             val rowsUpdated = App.application.databaseController.updateSchedule(response = it.syncResponse)
                             swipe_refresh!!.isRefreshing = false
 
-                            if( rowsUpdated == 0 )
+                            if (rowsUpdated == 0)
                                 Toast.makeText(context, "Up to date!", Toast.LENGTH_SHORT).show()
                             else if (rowsUpdated > 0) {
                                 App.application.postBusEvent(SyncResponseEvent(rowsUpdated))
