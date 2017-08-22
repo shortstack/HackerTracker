68473390c49ad642b6ca234e8d0e99c170ee4c88
Chris <disenchanted@live.ca>
2017-08-21 20:02:50 -0700

    Creating FAQ Object

---
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Model/FAQ.kt
@@ -0,0 +1,3 @@
+package com.shortstack.hackertracker.Model
+
+data class FAQ(val question: String, val answer: String)
--- hackertracker/src/main/java/com/shortstack/hackertracker/Renderer/FAQRenderer.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Renderer/FAQRenderer.kt
@@ -4,22 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pedrogomez.renderers.Renderer
+import com.shortstack.hackertracker.Model.FAQ
 import com.shortstack.hackertracker.R
 import kotlinx.android.synthetic.main.row_faq.view.*
 
-class FAQRenderer : Renderer<Array<String>>() {
+class FAQRenderer : Renderer<FAQ>() {
 
     override fun inflate(inflater: LayoutInflater, parent: ViewGroup): View {
         return inflater.inflate(R.layout.row_faq, parent, false)
     }
 
     override fun render(payloads: List<Any>) {
-        rootView.question.text = content[POS_QUESTION]
-        rootView.answer.text = content[POS_ANSWER]
-    }
-
-    companion object {
-        val POS_QUESTION = 0
-        val POS_ANSWER = 1
+        rootView.question.text = content.question
+        rootView.answer.text = content.answer
     }
 }
