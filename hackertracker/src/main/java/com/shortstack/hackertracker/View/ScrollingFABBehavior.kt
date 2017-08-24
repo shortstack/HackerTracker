e2babf041941a0fc52ed032b583953b32bf4b71e
Chris <disenchanted@live.ca>
2017-08-24 02:25:23 -0700

    Converting Views to Kotlin

---
+++ hackertracker/src/main/java/com/shortstack/hackertracker/View/ScrollingFABBehavior.kt
@@ -0,0 +1,42 @@
+package com.shortstack.hackertracker.View
+
+
+import android.content.Context
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.FloatingActionButton
+import android.util.AttributeSet
+import android.view.View
+import com.shortstack.hackertracker.R
+
+class ScrollingFABBehavior(context : Context, attrs : AttributeSet) : CoordinatorLayout.Behavior<FloatingActionButton>(context, attrs) {
+    private val toolbarHeight : Int
+
+    init {
+        this.toolbarHeight = getToolbarHeight(context)
+    }
+
+    override fun layoutDependsOn(parent : CoordinatorLayout, fab : FloatingActionButton, dependency : View) : Boolean {
+        return dependency is AppBarLayout
+    }
+
+    override fun onDependentViewChanged(parent : CoordinatorLayout, fab : FloatingActionButton, dependency : View) : Boolean {
+        if (dependency is AppBarLayout) {
+            val lp = fab.layoutParams as CoordinatorLayout.LayoutParams
+            val fabBottomMargin = lp.bottomMargin
+            val distanceToScroll = fab.height + fabBottomMargin
+            val ratio = dependency.getY().toFloat() / toolbarHeight.toFloat()
+            fab.translationY = -distanceToScroll * ratio
+        }
+        return true
+    }
+
+    private fun getToolbarHeight(context : Context) : Int {
+        val styledAttributes = context.theme.obtainStyledAttributes(
+                intArrayOf(R.attr.actionBarSize))
+        val toolbarHeight = styledAttributes.getDimension(0, 0f).toInt()
+        styledAttributes.recycle()
+
+        return toolbarHeight
+    }
+}
