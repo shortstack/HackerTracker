81ca7c06b60704a32c8d95cac346f527280d5cfc
Chris <disenchanted@live.ca>
2017-08-24 02:25:59 -0700

    Update Service to Kotlin

---
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Service/UpdateDatabaseService.kt
@@ -0,0 +1,14 @@
+package com.shortstack.hackertracker.Service
+
+import android.app.IntentService
+import android.content.Intent
+import com.shortstack.hackertracker.Application.App
+import com.shortstack.hackertracker.Event.SetupDatabaseEvent
+
+class UpdateDatabaseService : IntentService("DEFCONUpdateDatabaseService") {
+
+    override fun onHandleIntent(intent : Intent?) {
+        val databaseController = App.application.databaseController
+        App.application.postBusEvent(SetupDatabaseEvent())
+    }
+}
