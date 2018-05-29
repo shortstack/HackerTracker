9f1848f22d61ef402fa27a2f229909fa87e22d2a
Advice-Dog <disenchanted@live.ca>
2018-05-28 23:54:21 -0700

    Fixing FAQ and ConstraintSet animation

--- hackertracker/src/main/java/com/shortstack/hackertracker/ui/information/renderers/FAQRenderer.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/ui/information/renderers/FAQRenderer.kt
@@ -5,8 +5,11 @@ import android.support.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.OvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import com.firebase.jobdispatcher.Constraint
 import com.pedrogomez.renderers.Renderer
 import com.shortstack.hackertracker.R
 import com.shortstack.hackertracker.models.FAQ
@@ -22,25 +25,28 @@ class FAQRenderer : Renderer<FAQ>() {
         rootView?.setOnClickListener {
             val root = rootView.container
 
-            val visibility = if(rootView.answer.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+            val isExpanded = rootView.answer.visibility == View.VISIBLE
+
+
+            val visibility = if (isExpanded) View.GONE else View.VISIBLE
+
             val constraintSet1 = ConstraintSet()
             constraintSet1.clone(root)
 
-//            val constraintSet2 = ConstraintSet()
-//            constraintSet2.clone(context, R.layout.row_faq_expanded)
-
             constraintSet1.setVisibility(R.id.answer, visibility)
 
-
             val transition = ChangeBounds()
-            transition.interpolator = OvershootInterpolator()
-            TransitionManager.beginDelayedTransition(root, transition)
+            transition.interpolator = AccelerateDecelerateInterpolator()
 
+
+            TransitionManager.beginDelayedTransition(root, transition)
             constraintSet1.applyTo(root)
         }
     }
 
     override fun render(payloads: List<Any>) {
+        rootView?.answer?.visibility = View.GONE
+
         rootView.question.text = content.question
         rootView.answer.text = content.answer
     }
