bdf9f039079c61dd6471eebb471ab3577e4a3409
Advice-Dog <disenchanted@live.ca>
2018-06-03 23:46:21 -0700

    Cleaning Constants

--- hackertracker/src/main/java/com/shortstack/hackertracker/Constants.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Constants.kt
@@ -1,21 +1,20 @@
 package com.shortstack.hackertracker
 
-interface Constants {
-    companion object {
+object Constants {
 
-        const val API_URL_BASE = "https://info.defcon.org/json/"
-        const val API_GITHUB_BASE = "https://rawgit.com/BeezleLabs/conferences/feature/restructure/"
+    const val API_URL_BASE = "https://info.defcon.org/json/"
+    const val API_GITHUB_BASE = "https://rawgit.com/BeezleLabs/conferences/feature/restructure/"
 
-        const val TIME_SECONDS_IN_HOUR = 3600
+    const val TIME_SECONDS_IN_HOUR = 3600
 
-        const val DEBUG_FORCE_TIME_DATE = 1501355835000L
+    private const val AUG_29_2017_8AM = 1501340400000L
+    const val DEBUG_FORCE_TIME_DATE = AUG_29_2017_8AM
 
-        private const val TIMER_INTERVAL_FIVE_MIN: Long = 300000
-        private const val TIMER_INTERVAL_DEBUG: Long = 5000
+    private const val TIMER_INTERVAL_FIVE_MIN: Long = 300000
+    private const val TIMER_INTERVAL_DEBUG: Long = 5000
 
-        val TIMER_INTERVAL = if (BuildConfig.DEBUG) TIMER_INTERVAL_DEBUG else TIMER_INTERVAL_FIVE_MIN
+    val TIMER_INTERVAL = if (BuildConfig.DEBUG) TIMER_INTERVAL_DEBUG else TIMER_INTERVAL_FIVE_MIN
 
-        // TODO: Replace this with a feedback email.
-        const val FEEDBACK_EMAIL = "chrisporter0111@gmail.com"
-    }
+    // TODO: Replace this with a feedback email.
+    const val FEEDBACK_EMAIL = "chrisporter0111@gmail.com"
 }
