cc25d86a200b64145ac6d45e55b323f19e3a3600
Advice-Dog <disenchanted@live.ca>
2018-07-22 22:04:41 -0700

    Hardcoding PDF maps

--- hackertracker/src/main/java/com/shortstack/hackertracker/ui/maps/MapsFragment.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/ui/maps/MapsFragment.kt
@@ -24,17 +24,27 @@ class MapsFragment : androidx.fragment.app.Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tab_layout.apply {
+            tabGravity = com.google.android.material.tabs.TabLayout.GRAVITY_FILL
+            setupWithViewPager(pager)
+        }
+
+        val list = listOf(
+                ConferenceMap("Caesars", DC_26_CAESARS),
+                ConferenceMap("Flamingo Day", DC_26_FLAMINGO),
+                ConferenceMap("Flamingo Night", DC_26_FLAMINGO_NIGHT),
+                ConferenceMap("LINQ", DC_26_LINQ))
+
+        val adapter = PagerAdapter(activity!!.supportFragmentManager, list)
+        pager.adapter = adapter
+
+
         val mapsViewModel = ViewModelProviders.of(this).get(MapsViewModel::class.java)
         mapsViewModel.maps.observe(this, Observer {
             if (it != null) {
-                val adapter = PagerAdapter(activity!!.supportFragmentManager, it)
-                pager.adapter = adapter
 
                 if (it.size > 1) {
-                    tab_layout.apply {
-                        tabGravity = com.google.android.material.tabs.TabLayout.GRAVITY_FILL
-                        setupWithViewPager(pager)
-                    }
+                    tab_layout.visibility = View.VISIBLE
                 } else {
                     tab_layout.visibility = View.GONE
                 }
@@ -52,6 +62,12 @@ class MapsFragment : androidx.fragment.app.Fragment() {
     }
 
     companion object {
+
+        private const val DC_26_CAESARS = "dc-26-caesars-public-1.pdf"
+        private const val DC_26_FLAMINGO = "dc-26-flamingo-public-1.pdf"
+        private const val DC_26_FLAMINGO_NIGHT = "dc-26-flamingo-noct-public.pdf"
+        private const val DC_26_LINQ = "dc-26-linq-workshops.pdf"
+
 
         fun newInstance() = MapsFragment()
 
