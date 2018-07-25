caed3f794c8c6b5dabf4a78ab0d2f26399175b2e
Advice-Dog <disenchanted@live.ca>
2018-07-25 15:22:23 -0700

    Updating vendor model

--- hackertracker/src/main/java/com/shortstack/hackertracker/models/Vendor.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/models/Vendor.kt
@@ -4,17 +4,20 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(foreignKeys = [(ForeignKey(entity = (Conference::class), parentColumns = [("code")], childColumns = [("conference")], onDelete = ForeignKey.CASCADE))])
 data class Vendor(
-        @PrimaryKey(autoGenerate = true)
-        val index: Int,
-        val title: String,
-        val description: String,
-        val link: String,
-        val partner: Int,
+        @PrimaryKey
+        val id: Int,
+        val name: String,
+        val description: String?,
+        val link: String?,
+        val partner: Boolean,
+        @SerializedName("updated_at")
+        val updatedAt: String,
         val conference: String
 ) : Parcelable
 
