989f16c3ed4881997774e9715094daa33228d4d0
Advice-Dog <disenchanted@live.ca>
2018-07-25 01:01:06 -0700

    Updating FilterAdapter to support headers

--- hackertracker/src/main/java/com/shortstack/hackertracker/views/FilterAdapter.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/views/FilterAdapter.kt
@@ -13,47 +13,75 @@ import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.item_type.view.*
+import kotlinx.android.synthetic.main.item_type_header.view.*
 
 /**
  * Created by Chris on 7/12/2018.
  */
-class FilterAdapter(private val types: List<Type>, private val database: DatabaseManager) : RecyclerView.Adapter<FilterAdapter.ViewHolder>() {
+class FilterAdapter(private val types: List<Type>, private val database: DatabaseManager) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    companion object {
+        const val TYPE_HEADER = 2
+        const val TYPE_ITEM = 1
+    }
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_type, parent, false)
-        return ViewHolder(view)
+    class HeaderViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_HEADER -> HeaderViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_type_header, parent, false))
+            else -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_type, parent, false))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 1) {
+            TYPE_HEADER
+        } else {
+            TYPE_ITEM
+        }
     }
 
     override fun getItemCount() = types.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val type = types[position]
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> {
+                holder.view.apply {
+                    // TODO: Use the actual label.
+                    header.text = "Types"
+                }
+            }
+            is ViewHolder -> {
+                // TODO: This needs to be removed.
+                val type = types[if (position == 0) position else position - 1]
 
-        holder.view.apply {
-            val color = Color.parseColor(type.color)
+                holder.view.apply {
+                    val color = Color.parseColor(type.color)
 
-            chip.chipText = type.name
-            chip.chipBackgroundColor = ColorStateList.valueOf(color)
-            chip.isCloseIconEnabled = type.isSelected
+                    chip.chipText = type.name
+                    chip.chipBackgroundColor = ColorStateList.valueOf(color)
+                    chip.isCloseIconEnabled = type.isSelected
 
-            chip.setOnCheckedChangeListener(null)
+                    chip.setOnCheckedChangeListener(null)
 
-            chip.isChecked = type.isSelected
+                    chip.isChecked = type.isSelected
 
 
-            chip.setOnCheckedChangeListener { _, isChecked ->
+                    chip.setOnCheckedChangeListener { _, isChecked ->
 
-                chip.isCloseIconEnabled = isChecked
+                        chip.isCloseIconEnabled = isChecked
 
-                Single.fromCallable {
-                    type.isSelected = isChecked
-                    database.updateTypeIsSelected(type)
-                }.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({}, {})
+                        Single.fromCallable {
+                            type.isSelected = isChecked
+                            database.updateTypeIsSelected(type)
+                        }.subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe({}, {})
+                    }
+                }
             }
         }
     }
--- hackertracker/src/main/java/com/shortstack/hackertracker/views/FilterView.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/views/FilterView.kt
@@ -14,37 +14,30 @@ import javax.inject.Inject
 
 class FilterView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
-
     @Inject
     lateinit var database: DatabaseManager
-
-    private lateinit var adapter: FilterAdapter
 
     init {
         App.application.component.inject(this)
         View.inflate(context, R.layout.view_filter, this)
-
-
-    }
-
-    private fun setCheckboxes(types: List<Type>) {
-
-        val local = mutableListOf<Type>()
-//        for(i in 0 until 20 )
-            local.addAll(types)
-
-        adapter = FilterAdapter(local, database)
-
-        list.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
-
-        list.adapter = adapter
     }
 
     fun setTypes(types: List<Type>?) {
+        if (types != null) {
+            val adapter = FilterAdapter(types, database)
 
-        if (types != null)
-            setCheckboxes(types)
+            list.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false).apply {
+                spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return when (adapter.getItemViewType(position)) {
+                            FilterAdapter.TYPE_HEADER -> 2
+                            else -> 1
+                        }
+                    }
+                }
+            }
+            list.adapter = adapter
+
+        }
     }
-
-
 }
