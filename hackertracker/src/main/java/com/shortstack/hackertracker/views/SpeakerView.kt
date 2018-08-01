3b626887c2fcdb49537f92c2f71e61667edd737d
Advice-Dog <disenchanted@live.ca>
2018-07-31 20:23:12 -0700

    Removing speaker initials

--- hackertracker/src/main/java/com/shortstack/hackertracker/views/SpeakerView.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/views/SpeakerView.kt
@@ -15,7 +15,6 @@ class SpeakerView(context: Context, speaker: Speaker) : LinearLayout(context) {
     }
 
     private fun render(speaker: Speaker) {
-        speaker_initials.text = speaker.name.trim().split(" ").joinToString("") { it.first().toString() }
         speaker_name.text = speaker.name
         speaker_description.text = speaker.title ?: context.getString(R.string.speaker_default_title)
     }
