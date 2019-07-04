8539f026ccf32cce139a9df8f7b092912cad51ab
Chris P <disenchanted@live.ca>
2019-07-04 13:13:27 -0700

    Updating the feedback email

--- hackertracker/src/main/java/com/shortstack/hackertracker/Constants.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Constants.kt
@@ -2,6 +2,5 @@ package com.shortstack.hackertracker
 
 object Constants {
 
-    // TODO: Replace this with a feedback email.
-    const val FEEDBACK_EMAIL = "chrisporter0111@gmail.com"
+    const val FEEDBACK_EMAIL = "hackertracker@defcon.org"
 }
