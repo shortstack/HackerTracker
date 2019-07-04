11a1a12a52a7ef9766e6b53819548d60bbbccda4
Chris P <disenchanted@live.ca>
2019-07-04 11:40:59 -0700

    Removing isSoonish

--- hackertracker/src/main/java/com/shortstack/hackertracker/Extensions.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Extensions.kt
@@ -33,20 +33,6 @@ fun Date.isTomorrow(): Boolean {
     return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)
 }
 
-fun Date.isSoonish(SOON_DAYS_AMOUNT: Int): Boolean {
-    val cal1 = Calendar.getInstance().now()
-
-    val time = cal1.time.time
-
-    val cal2 = Calendar.getInstance()
-    cal2.time = this
-
-    val time2 = cal2.time.time
-
-    val daysInMilliSeconds = SOON_DAYS_AMOUNT * 1000 * 60 * 60 * 24
-    return time2 - time < daysInMilliSeconds
-}
-
 fun Date.getDateDifference(date: Date, timeUnit: TimeUnit): Long {
     return timeUnit.convert(date.time - this.time, TimeUnit.MILLISECONDS)
 }
