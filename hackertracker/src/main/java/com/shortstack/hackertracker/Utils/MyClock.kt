87ab5f744daac0776f90278994ac2a4b3253b129
Chris P <disenchanted@live.ca>
2019-07-23 13:26:44 -0700

    Adding Articles

--- hackertracker/src/main/java/com/shortstack/hackertracker/Utils/MyClock.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/Utils/MyClock.kt
@@ -1,9 +1,18 @@
 package com.shortstack.hackertracker.utils
 
+import com.shortstack.hackertracker.BuildConfig
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class MyClock(val value: Int = 0)
 
 fun MyClock.now(): Date {
+    if(BuildConfig.DEBUG) {
+        return parse("2019-06-01T12:00:00.000-0000")
+    }
     return Date()
+}
+
+private fun parse(date: String): Date {
+    return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").parse(date)
 }
