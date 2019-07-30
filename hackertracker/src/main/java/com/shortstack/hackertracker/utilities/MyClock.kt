d34b9d21361419eb7cb721ae40f857c9cb6fbc9a
Chris P <disenchanted@live.ca>
2019-07-30 14:15:47 -0700

    Removing MyClock

--- hackertracker/src/main/java/com/shortstack/hackertracker/utilities/MyClock.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/utilities/MyClock.kt
@@ -1,15 +1,9 @@
 package com.shortstack.hackertracker.utilities
 
-import com.shortstack.hackertracker.BuildConfig
-import java.text.SimpleDateFormat
 import java.util.*
 
 data class MyClock(val value: Int = 0)
 
 fun MyClock.now(): Date {
     return Date()
-}
-
-private fun parse(date: String): Date {
-    return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").parse(date)
 }
