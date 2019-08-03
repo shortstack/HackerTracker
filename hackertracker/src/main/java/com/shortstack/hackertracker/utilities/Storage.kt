a048f8fb88bdf51baf4a99e8630aa7ba400e8cb7
Chris <chrisporter0111@gmail.com>
2019-08-03 14:49:19 -0700

    Adding storage for nav drawer and force time zone.

--- hackertracker/src/main/java/com/shortstack/hackertracker/utilities/Storage.kt
+++ hackertracker/src/main/java/com/shortstack/hackertracker/utilities/Storage.kt
@@ -10,6 +10,9 @@ class Storage(context: Context) {
         private const val USER_THEME = "user_theme"
         private const val USER_CONFERENCE = "user_conference"
 
+        private const val NAV_DRAWER_ON_BACK = "nav_drawer_on_back"
+        private const val FORCE_TIME_ZONE = "force_time_zone"
+
         private const val USER_ALLOW_PUSH = "user_allow_push_notifications"
         private const val USER_ANALYTICS = "user_analytics"
     }
@@ -25,6 +28,18 @@ class Storage(context: Context) {
             preferences.edit().putBoolean(USER_ANALYTICS, value).apply()
         }
 
+    var navDrawerOnBack: Boolean
+        get() = preferences.getBoolean(NAV_DRAWER_ON_BACK, false)
+        set(value) {
+            preferences.edit().putBoolean(NAV_DRAWER_ON_BACK, value).apply()
+        }
+
+    var forceTimeZone: Boolean
+        get() = preferences.getBoolean(FORCE_TIME_ZONE, true)
+        set(value) {
+            preferences.edit().putBoolean(FORCE_TIME_ZONE, value).apply()
+        }
+
 
     var preferredConference: Int
         get() = preferences.getInt(USER_CONFERENCE, -1)
@@ -35,6 +50,8 @@ class Storage(context: Context) {
     fun setPreference(key: String, isChecked: Boolean) {
         when (key) {
             USER_ANALYTICS -> allowAnalytics = isChecked
+            NAV_DRAWER_ON_BACK -> navDrawerOnBack = isChecked
+            FORCE_TIME_ZONE -> forceTimeZone = isChecked
             else -> throw IllegalArgumentException("Unknown key: $key")
         }
     }
