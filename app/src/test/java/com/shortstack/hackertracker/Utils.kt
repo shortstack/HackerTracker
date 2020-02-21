efd159edf06f69a0610f7580c4df51d99947bbce
Chris P <disenchanted@live.ca>
2020-02-21 10:28:37 -0800

    Fixing and adding more tests to DatabaseManager

--- app/src/test/java/com/shortstack/hackertracker/Utils.kt
+++ app/src/test/java/com/shortstack/hackertracker/Utils.kt
@@ -1,5 +1,6 @@
 package com.shortstack.hackertracker
 
+import com.google.firebase.Timestamp
 import com.shortstack.hackertracker.utilities.MyClock
 import com.shortstack.hackertracker.utilities.now
 import io.mockk.every
@@ -16,4 +17,8 @@ fun setCurrentClock(date: String) {
 
 fun parse(date: String): Date {
     return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").parse(date)
+}
+
+fun fromString(date: String): Timestamp {
+    return Timestamp(SimpleDateFormat("yyyy-MM-dd").parse(date))
 }
--- app/src/test/java/com/shortstack/hackertracker/database/DatabaseManagerTest.kt
+++ app/src/test/java/com/shortstack/hackertracker/database/DatabaseManagerTest.kt
@@ -1,5 +1,6 @@
 package com.shortstack.hackertracker.database
 
+import com.shortstack.hackertracker.fromString
 import com.shortstack.hackertracker.models.firebase.FirebaseConference
 import com.shortstack.hackertracker.models.local.Conference
 import com.shortstack.hackertracker.setCurrentClock
@@ -10,9 +11,9 @@ import org.junit.Test
 class DatabaseManagerTest {
 
     private val conferences: List<Conference> = listOf(
-            FirebaseConference(code = "abc", start_date = "2019-01-01", end_date = "2019-01-01"),
-            FirebaseConference(code = "123", start_date = "2019-03-02", end_date = "2019-03-03"),
-            FirebaseConference(code = "456", start_date = "2019-05-02", end_date = "2019-05-03")
+            FirebaseConference(id = 1, code = "abc", start_timestamp = fromString("2019-01-01"), end_timestamp = fromString("2019-01-01")),
+            FirebaseConference(id = 2, code = "123", start_timestamp = fromString("2019-03-02"), end_timestamp = fromString("2019-03-03")),
+            FirebaseConference(id = 3, code = "456", start_timestamp = fromString("2019-05-02"), end_timestamp = fromString("2019-05-03"))
     ).map { it.toConference() }
 
     @Test
@@ -25,10 +26,65 @@ class DatabaseManagerTest {
     }
 
     @Test
+    fun `get preferred conference` (){
+        setCurrentClock("2019-01-01T11:00:00.000-0000")
+
+        val result = DatabaseManager.getNextConference(2, conferences)
+
+        assertEquals("123", result?.code)
+    }
+
+    @Test
+    fun `get defcon conference` (){
+        setCurrentClock("2019-01-01T11:00:00.000-0000")
+
+        val defcon = FirebaseConference(
+            id = 4,
+            code = "DEFCON27",
+            start_timestamp = fromString("2019-05-02"),
+            end_timestamp = fromString("2019-05-03")
+        ).toConference()
+
+        val conferences = conferences + defcon
+
+        val result = DatabaseManager.getNextConference(-1, conferences)
+
+        assertEquals("DEFCON27", result?.code)
+    }
+
+    @Test
+    fun `skip defcon conference when finished` (){
+        setCurrentClock("2019-03-03T11:00:00.000-0000")
+
+        val defcon = FirebaseConference(
+            id = 4,
+            code = "DEFCON27",
+            start_timestamp = fromString("2019-02-02"),
+            end_timestamp = fromString("2019-02-03")
+        ).toConference()
+
+        val conferences = conferences + defcon
+
+        val result = DatabaseManager.getNextConference(-1, conferences)
+
+        assertEquals("456", result?.code)
+    }
+
+
+    @Test
     fun `get newest conference when all finished` (){
         setCurrentClock("2019-06-01T11:00:00.000-0000")
 
         val result = DatabaseManager.getNextConference(-1, conferences)
+
+        assertEquals("456", result?.code)
+    }
+
+    @Test
+    fun `get newest conference when all finished with preference` (){
+        setCurrentClock("2019-06-01T11:00:00.000-0000")
+
+        val result = DatabaseManager.getNextConference(1, conferences)
 
         assertEquals("456", result?.code)
     }
