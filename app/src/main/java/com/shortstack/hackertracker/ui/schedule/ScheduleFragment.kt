b99ebbc009afdae670cb54b218dc4eb28beab3f8
Chris <chrisporter0111@gmail.com>
2022-06-02 10:49:18 -0700

    Hiding schedule title

--- app/src/main/java/com/shortstack/hackertracker/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/shortstack/hackertracker/ui/schedule/ScheduleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
@@ -56,6 +57,7 @@ class ScheduleFragment : Fragment() {
 
         if (type != null || location != null) {
             binding.toolbar.title = type?.shortName ?: location?.name
+            binding.title.visibility = View.GONE
             binding.filter.visibility = View.GONE
             binding.toolbar.invalidateMenu()
             binding.toolbar.navigationIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_arrow_back_ios_new_24)
