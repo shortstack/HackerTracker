7b4bcaf4b554013eae5d1013f00c3649d0598f5a
Chris <chrisporter0111@gmail.com>
2022-07-09 16:41:44 -0700

    Hiding countdown when time is zero

--- app/src/main/java/com/advice/schedule/views/HeaderView.kt
+++ app/src/main/java/com/advice/schedule/views/HeaderView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import com.shortstack.hackertracker.databinding.HeaderHomeBinding
 
 class HeaderView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
@@ -11,6 +12,7 @@ class HeaderView(context: Context, attrs: AttributeSet?) : ConstraintLayout(cont
     private val binding = HeaderHomeBinding.inflate(LayoutInflater.from(context), this, true)
 
     fun setCountdown(time: Long) {
+        binding.countdown.isVisible = time > 0
         binding.countdown.setCountdown(time)
     }
 }
