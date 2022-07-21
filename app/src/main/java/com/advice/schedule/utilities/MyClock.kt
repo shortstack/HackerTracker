fb3629cb07b4af3665be2b9703397ca248da78ff
Chris <chrisporter0111@gmail.com>
2022-07-20 23:25:00 -0700

    Update MyClock.kt

--- app/src/main/java/com/advice/schedule/utilities/MyClock.kt
+++ app/src/main/java/com/advice/schedule/utilities/MyClock.kt
@@ -16,3 +16,26 @@ fun MyClock.now(): Date {
 private fun parse(date: String): Date {
     return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").parse(date)
 }
+
+
+fun getDateMidnight(date: Date, zone: String? = null): Date {
+    val apply = Calendar.getInstance().apply {
+
+
+        time = date
+
+        if (zone != null) {
+//        if (App.instance.storage.forceTimeZone) {
+//            val timezone =
+//                App.instance.database.conference.value?.timezone ?: "America/Los_Angeles"
+            timeZone = TimeZone.getTimeZone(zone)
+//        }
+        }
+
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+    return apply.time
+}
